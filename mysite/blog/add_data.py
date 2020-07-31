f0363aee04c9a33d34a325ac26a3ed6b791bb9c2
gongshuo <470690988@qq.com>
2020-07-31 17:52:36 +0800

    提交-3

---
+++ mysite/blog/add_data.py
@@ -0,0 +1,15 @@
+from . import  models
+from django.contrib.auth.models import User
+
+
+def add_data1(): # blog_articles 插入数据
+    u = User.objects.filter(id=1).first()
+    a = models.BlogArticles(author=u, title='hello', body='lalala')
+    a.save()
+    """以下为参考代码"""
+    # d1 = DepartmentInfo.objects.get(depart_id=1)  # d1表示UserInfo的外键数据
+    # r1 = Role.objects.get(role_name=role)  # r1表示UserInfo的多对多数据
+    # u1 = UserInfo(user_name=name, user_pwd=password, sex=sex, mobileno=mobile, email=email, depart=d1)
+    # u1.save()
+    # u1.role.add(r1)  # 多对多关系
+    # u1.save()
